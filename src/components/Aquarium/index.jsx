ca4465337058f56cc2ccb9782c9ff18557729348
Muhammad Awwad <hammode.awad@gmail.com>
2020-11-25 01:20:02 +0200

    We worked on showing the fish inside the aquarium according to the array, and we used some nice animations to make the movemoent look greaaat

--- src/components/Aquarium/index.jsx
+++ src/components/Aquarium/index.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
-
-import './style.css'
+import React from "react";
+import "./style.css";
 
 const Aquarium = (props) => {
-    return 'Aquarium'
-}
+  //This function returns a random number between two specific numbers
+  function randomNumBetweenTwo(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-export default Aquarium
+  //This function returns a random num between 0 and n
+  function randomNum(n) {
+    return Math.floor(Math.random() * n + 1);
+  }
+
+  return (
+    <div className="aquarium">
+      <img
+        src="/images/fish.gif"
+        className="fish"
+        style={{
+          animationDuration: randomNumBetweenTwo(3, 10) + "s",
+          marginTop: randomNum(500) + "px",
+        }}
+      />
+      <img
+        src="/images/fish.gif"
+        className="fish"
+        style={{
+          animationDuration: randomNumBetweenTwo(3, 10) + "s",
+          marginTop: randomNum(500) + "px",
+        }}
+      />
+      <img
+        src="/images/fish.gif"
+        className="fish"
+        style={{
+          animationDuration: randomNumBetweenTwo(3, 10) + "s",
+          marginTop: randomNum(500) + "px",
+        }}
+      />
+      <img
+        src="/images/fish.gif"
+        className="fish"
+        style={{
+          animationDuration: randomNumBetweenTwo(3, 10) + "s",
+          marginTop: randomNum(500) + "px",
+        }}
+      />
+      <img
+        src="/images/fish.gif"
+        className="fish"
+        style={{
+          animationDuration: randomNumBetweenTwo(3, 10) + "s",
+          marginTop: randomNum(500) + "px",
+        }}
+      />
+      <img
+        src="/images/fish.gif"
+        className="fish"
+        style={{
+          animationDuration: randomNumBetweenTwo(3, 10) + "s",
+          marginTop: randomNum(500) + "px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Aquarium;
